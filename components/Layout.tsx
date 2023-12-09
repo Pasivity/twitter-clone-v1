5ed6f4338fbaecbca8eeee46720e4252eb2e1f5e
Pasivity <www.rafa.lopex.11@gmail.com>
2023-12-08 22:37:11 -0800

    u1pdate

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import Sidebar from "./Layout/Sidebar";
+import FollowBar from "./Layout/FollowBar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -6,8 +8,16 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      {children}deededede
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+          <FollowBar />
+        </div>
+      </div>
     </div>
   );
 };
